7cf7faa774d5291bd30427ddba87ccd2beee7b0f
adam9617173 <adam9617173@gmail.com>
2021-03-23 17:50:12 +0800

    Add files via upload

--- Demo.py
+++ Demo.py
@@ -1,6 +1,41 @@
 class Crypto:
-    def __init__(self, key):
-        self.key = key
+    def __init__(self, style = None, key = None):
+        self.key = list(key)
+        self.style = style
+        self.msg = None
+        self.ans = None
     
-    def encrypt(self, msg):
-        print("hello")
+    def Encrypt(self, msg):
+        if self.style == "XOR":
+            self.msg = list(msg)
+            save = list()
+            for i in range(len(self.msg)):
+                save.append(int(self.msg[i])^int(self.key[i]))
+
+            self.ans = save
+
+        elif  self.style == "Caesar":
+            self.msg = list(msg)
+            key_len = len(self.key)
+            save = list()
+            for i in range(len(self.msg)):
+                save.append(chr(ord(self.msg[i])+int(self.key[i%key_len])))    
+            self.ans = save
+
+    def Decrypt(self, ans):
+        if style == "XOR":
+            self.ans = list(ans)
+            save = list()
+            for i in range(len(self.ans)):
+                save.append((int(self.ans[i])^int(self.key[i])))
+
+            self.msg = save        
+        elif style == "Caesar":
+            key_len = len(self.key)    
+
+
+    def lists(self):
+        print("now keys is : ",self.key)
+        print("now msg is : ", self.msg)
+        print("now ans is : ", self.ans)
+
--- xor_text.py
+++ xor_text.py
@@ -1,10 +1,34 @@
-m = int(input("input message : "))
-k = int(input("input key : "))
-num = m^k
-print(num)
-print(bin(num))
+import Demo
 
-m = "01010011"
-k = "00111000"
-for i in range(len(m)):
-    m[i]
+#key = 00111000
+#msg = 01010011
+#ans = 01101011
+
+# dm = Demo.Crypto("00111000")
+# print(dm.Encrypt("01010011"))
+# print(dm.Decrypt("01101011"))
+
+# a = input("Please input your keys first : ")
+# demo = Demo.Crypto(a)
+while(1):
+    style = input("Choose Crypt or check list? \nPlease input XOR or Caesar or L (List): ")
+    if style == "XOR" or style == "Caesar":
+        key = input("Please input you crypt's key : ")
+        demo = Demo.Crypto(style,key)
+    elif style == "L" or style == "l":
+        demo.lists()
+        continue
+
+    menu = input("\n\nE = encrypt \n\nD = Decrypt \n\nL = List \n\nQ = Quit \n\nPlease input the funtoin u want to use : ")
+    if menu == "E" or menu == "e":
+        msg = input("Please input your msg : ")
+        demo.Encrypt(msg)
+
+    elif menu == "D" or menu == "d":
+        ans = input("Please input your ans : ")
+        demo.Decrypt(ans)
+
+    
+
+    if menu == "Q" or menu == "q":
+        break
